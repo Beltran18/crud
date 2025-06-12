51e9eae0e4d8bd5fd2c37234fd2d194fe6838384
Beltran18 <miguelbelttan12@gmail.com>
2025-06-12 00:01:16 -0500

    creacion archivos y primer paso de mi crud

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="datos">
+      <label>nombre: <input type='text'></input></label>
+      </div>
+    </div>
+  );
+}
+
+export default App;
